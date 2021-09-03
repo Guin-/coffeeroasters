af4b9257ed6ebb50254b703f78760e363f584648
Angelika Jarosz <angelikajarosz@gmail.com>
2021-09-03 11:08:02 -0600

    create files for components, basic routing for 3 main pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+        BrowserRouter as Router,
+        Switch,
+        Route,
+} from 'react-router-dom';
+import Navigation from './components/Navigation';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import About from './components/About';
+import Subscribe from './components/Subscribe';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navigation/>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route path="/about" component={About}/>
+        <Route path="/subscribe" component={Subscribe}/>
+      </Switch>
+      <Footer/>
+    </Router>
   );
 }
 
